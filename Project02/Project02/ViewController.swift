4079fb08215b404819beb492a68a9ebb63c585d1
Ángel Vázquez <angel_betta@outlook.com>
2018-09-08 14:15:24 -0500

    Finished project 2. Updated README

--- Project02/Project02/ViewController.swift
+++ Project02/Project02/ViewController.swift
@@ -7,13 +7,72 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet var button1: UIButton!
+	@IBOutlet var button2: UIButton!
+	@IBOutlet var button3: UIButton!
+	
+	var countries = [String]()
+	var score = 0
+	
+	var correctAnswer = 0
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+		
+		//	Adding elements to the array using the append method.
+		countries.append("estonia")
+		countries.append("france")
+		countries.append("germany")
+		
+		//	Adding elements using the += operator of arrays. This strategy is more efficient because it uses only one line of code.
+		countries += ["ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+		
+		
+		//	Using CALayer to add a border to the buttons. The values are measured in points. (1 pixel in non-retina screens, 2 pixels in retina screens and 3 pixels in Retina HD screens)
+		button1.layer.borderWidth = 1
+		button2.layer.borderWidth = 1
+		button3.layer.borderWidth = 1
+		
+		button1.layer.borderColor = UIColor.lightGray.cgColor
+		button2.layer.borderColor = UIColor.lightGray.cgColor
+		button3.layer.borderColor = UIColor.lightGray.cgColor
+		
+		
+		askQuestion()
     }
+	
+	func askQuestion(action: UIAlertAction! = nil) {
+		
+		countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+		
+		button1.setImage(UIImage(named: countries[0]), for: .normal)
+		button2.setImage(UIImage(named: countries[1]), for: .normal)
+		button3.setImage(UIImage(named: countries[2]), for: .normal)
+		
+		correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+
+		title = countries[correctAnswer].uppercased()
+	}
+	
+	@IBAction func buttonTapped(_ sender: UIButton) {
+		var title: String
+		if sender.tag == correctAnswer {
+			title = "Correct"
+			score += 1
+		} else {
+			title = "Wrong"
+			score -= 1
+		}
+		
+		let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+		present(ac, animated: true)
+	}
+	
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ Project02/README.md
@@ -0,0 +1,37 @@
+
+# Project 2. Guess the Flag
+
+## Concepts
+
+- Interface Builder
+- @2x and @3x images
+- Asset Catalogs (`Assets.xcassets`)
+- `UIButton`
+- Enums
+- `CALayer` (Core Animation)
+- `UIColor` and `CGColor` (Core Graphics)
+- Random numbers (`GameplayKit`)
+- Actions (`@IBAction`) and outlets (`@IBOutlet`)
+- String interpolation (`let message = "Your score is \(score)"`)
+- `UIAlertController`
+
+## Notes
+
+### Pixels and Points
+
+Early iOS devices had non-retina screens. This meant a screen resolution of 320 x 480 pixels. So you could place things exactly where you wanted them.
+
+With iPhone 4, Apple introduced retina screens that had double the number of pixels as previous screens. Apple automatically switched sizes from pixels to **points** (virtual pixels). On non-retina devices, 10 points became 10 pixels, but on retina devices 10 points became 20 pixels. This changes guarantee that everything looked the same size and shape on both devices, with a single layout.
+
+Retina HD screens have a 3x resolution. 
+
+The AppStore uses a technology called **app thinning** so that it delivers only the content each device is capable of showing (it strips out other assets to reduce wasted space). As of iOS 10, no non-retina devices are supported.
+
+### `CALayer`
+
+Views in iOS are backed by what's called a `CALayer`, a Core Animation data type responsible for managing the way the view looks.
+
+### Alerts
+
+The `preferredStyle` parameter of `UIAlertController` initializers offers two styles: `.alert` and `.actionSheet`. The first one is used when you're telling users about a situation change. On the other hand, `.actionSheet` is best used when asking users to choose from a set of options.
+
