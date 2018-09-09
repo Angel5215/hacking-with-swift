922ca6805b4a6eb350d0272d80d79df9495f066d
Ángel Vázquez <angel_betta@outlook.com>
2018-09-08 22:29:41 -0500

    Initial version of Project06b with simple VFL constraints.

--- Project06/Project06b/Project06b/ViewController.swift
+++ Project06/Project06b/Project06b/ViewController.swift
@@ -9,11 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	
+	//	Hide the bar that shows what time it is.
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		let label1 = UILabel()
+		label1.translatesAutoresizingMaskIntoConstraints = false
+		label1.backgroundColor = .red
+		label1.text = "THESE"
+		label1.sizeToFit()
+		
+		let label2 = UILabel()
+		label2.translatesAutoresizingMaskIntoConstraints = false
+		label2.backgroundColor = UIColor.cyan
+		label2.text = "ARE"
+		label2.sizeToFit()
+		
+		let label3 = UILabel()
+		label3.translatesAutoresizingMaskIntoConstraints = false
+		label3.backgroundColor = .yellow
+		label3.text = "SOME"
+		label3.sizeToFit()
+		
+		let label4 = UILabel()
+		label4.translatesAutoresizingMaskIntoConstraints = false
+		label4.backgroundColor = UIColor.green
+		label4.text = "AWESOME"
+		label4.sizeToFit()
+		
+		let label5 = UILabel()
+		label5.translatesAutoresizingMaskIntoConstraints = false
+		label5.backgroundColor = .orange
+		label5.text = "LABELS"
+		label5.sizeToFit()
+		
+		view.addSubview(label1)
+		view.addSubview(label2)
+		view.addSubview(label3)
+		view.addSubview(label4)
+		view.addSubview(label5)
+		
+		//	Dictionary needed to use VFL
+		let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+		
+		//	VFL Constraints. For explanation about the syntax of VFL, consult the README.md file.
+		for label in viewsDictionary.keys {
+			
+			//	Add horizontal constraints using VFL.
+			view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+		}
+		
+		//	Add vertical constraints using VFL. 
+		view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+		
 	}
+
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
--- Project06/README.md
+++ Project06/README.md
@@ -1,4 +1,42 @@
 
 # Project 6. Auto Layout.
 
-This is actually divided into two different Xcode projects called `Project06a` and `Project06b`. The first one aims to fix [Project 2](./../Project02)
+This is actually divided into two different Xcode projects called `Project06a` and `Project06b`. The first one aims to fix [Project 2](./../Project02) to demonstrate more advanced Auto Layout techniques while also making the flags in the game stay on the screen correctly (even with landscape orientation). The second one shows Auto Layout in code.
+
+## Concepts in 6a.
+
+- Inequality constraint (>=, <=, ==)
+- Aspect ratio
+- Equal heights
+
+
+## Concepts in 6b
+
+- Auto Resize
+- NSLayoutConstraints
+- Auto Layout Visual Format Language (VFL)
+
+## Notes
+
+By default, iOS generates Auto Layout constraints for you based on a view's size and position. If we're specifying them by hand, we need to disable this feature setting the property `translatesAutoresizingMaskIntoConstraints` to `false`.
+
+We can specify constraints using **Auto Layout Visual Format Language (VFL)**, which is a way of "drawing" the layout you want with a series of keyboard symbols.
+
+### Example
+
+An example of a VFL string: `"H:|[label1]|"`.
+
+This string describes how we want the layout to look. The VFL string gets converted into Auto Layout constraints, then added to the view.
+
+* `H:`means we're defining a **horizontal** constraint.
+* `|`means **the edge of the view**
+* `[label1]` means put `label1` here.
+
+In other words, this means "horizontally, I want `label1` to to edge to edge in my view". What's `label1`? The label specified by the key `"label1"` in the `viewsDictionary`.
+
+
+
+
+
+
+
