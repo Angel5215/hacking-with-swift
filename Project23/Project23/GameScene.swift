77fc23850aa038024de0f81ab23721244a3eb77e
Ángel Vázquez <angel_betta@outlook.com>
2018-12-17 18:08:01 -0600

    Added improvements suggested in the last section

--- Project23/Project23/GameScene.swift
+++ Project23/Project23/GameScene.swift
@@ -26,7 +26,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var possibleEnemies = ["hammer", "ball", "tv"]
     var gameTimer: Timer!
     var isGameOver = false
-    
+	
+	//	Lasers
+	let LASER_PLAYER_OFFSET: CGFloat = 5
     
     //  MARK: Initial methods
     override func didMove(to view: SKView) {
@@ -40,6 +42,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         starfield.zPosition = -1
         
         player = SKSpriteNode(imageNamed: "player")
+		player.name = "player"
         player.position = CGPoint(x: 100, y: 384)
         //  Adds per-pixel collision detection.
         player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
@@ -71,17 +74,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
         sprite.physicsBody?.contactTestBitMask = 1
-        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.velocity = CGVector(dx: Int.random(in: -3000 ... -500), dy: 0)
         sprite.physicsBody?.angularVelocity = 5
         sprite.physicsBody?.linearDamping = 0
         sprite.physicsBody?.angularDamping = 0
+		sprite.name = "debris"
     }
     
     override func update(_ currentTime: TimeInterval) {
         for node in children {
             if node.position.x < -300 {
                 node.removeFromParent()
-            }
+			} else if node.position.x > 1300 {
+				node.removeFromParent()
+			}
         }
         
         if !isGameOver {
@@ -99,17 +105,71 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         } else if location.y > 668 {
             location.y = 668
         }
-        
-        player.position = location
+		
+		let xDifference = abs(player.position.x - location.x)
+		let yDifference = abs(player.position.y - location.y)
+		
+		if xDifference <= 30 && yDifference <= 30 {
+			player.position = location
+		}
+		
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        let explosion = SKEmitterNode(fileNamed: "explosion")!
-        explosion.position = player.position
-        addChild(explosion)
-        
-        player.removeFromParent()
-        
-        isGameOver = true
+		
+		guard let nodeA = contact.bodyA.node else { return }
+		guard let nodeB = contact.bodyB.node else { return }
+		
+		if nodeA.name == "player" && nodeB.name == "debris" {
+			gameOver()
+			print(1)
+		} else if nodeA.name == "debris" && nodeB.name == "player" {
+			gameOver()
+			print(2)
+		} else if nodeA.name == "laser" && nodeB.name == "debris" {
+			destroy(debrisNode: nodeB, by: nodeA)
+		} else if nodeA.name == "debris" && nodeB.name == "laser" {
+			destroy(debrisNode: nodeA, by: nodeB)
+		}
+		
     }
+	
+	func destroy(debrisNode: SKNode, by laserNode: SKNode) {
+		let explosion = SKEmitterNode(fileNamed: "debrisExplosion")!
+		explosion.position = debrisNode.position
+		addChild(explosion)
+		
+		laserNode.removeFromParent()
+		debrisNode.removeFromParent()
+	}
+	
+	func gameOver() {
+		let explosion = SKEmitterNode(fileNamed: "explosion")!
+		explosion.position = player.position
+		addChild(explosion)
+		
+		player.removeFromParent()
+		gameTimer.invalidate()
+		
+		isGameOver = true
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		
+		if isGameOver { return }
+		
+		let x = player.position.x + player.size.width / 2.0 + LASER_PLAYER_OFFSET
+		let y = player.position.y
+		let position = CGPoint(x: x, y: y)
+		
+		let laser = SKSpriteNode(imageNamed: "laser")
+		laser.name = "laser"
+		laser.position = position
+		laser.physicsBody = SKPhysicsBody(rectangleOf: laser.size)
+		laser.physicsBody?.velocity = CGVector(dx: 600, dy: 0)
+		laser.physicsBody?.linearDamping = 0
+		laser.physicsBody?.contactTestBitMask = 0x10
+		
+		addChild(laser)
+	}
 }
